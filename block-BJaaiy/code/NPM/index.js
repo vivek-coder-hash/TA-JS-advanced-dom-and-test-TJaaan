ae1aac91dff4fa2ade86562e2b3f429df291693b
vivek sharma <vivekslim@gmail.com>
2021-06-28 11:54:58 +0530

    npm

---
+++ block-BJaaiy/code/NPM/index.js
@@ -0,0 +1,13 @@
+/*const moment = require("moment")
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+console.log(moment().add(10, 'days').calendar())
+
+
+console.log(moment().add(7, 'months').calendar())*/
+
+
+const lodash = require("lodash")
+
+let array =  ["a" , "b" , "c" , "d"]
+
+console.log(lodash.chunk(array,3))
